d0e911c2faf262909fd307bc0d45e58071018b59
sergiuchilat <chilatsergiu@gmail.com>
2024-01-16 12:06:45 +0200

    Move files into JS Examples folder

---
+++ JS Examples/1 - JS Syntax/app.js
@@ -0,0 +1,14 @@
+alert("Hello world! I am a JS script:)");
+
+console.log('Hello world! I am a JS script:)')
+
+const name = prompt('What is your name?');
+
+alert(`Hello ${name}`)
+
+if(confirm('Do you like JS?')){
+    alert('Great!')
+} else{
+    alert('...');
+}
+
